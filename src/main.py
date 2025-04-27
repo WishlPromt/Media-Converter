0d0ec52022844a9ae2c8526b6a9f908f1639b326
Wishl Promt <igorbedniagin1@gmail.com>
2025-04-27 18:30:18 +0500

    Added parser

--- src/main.py
+++ src/main.py
@@ -1,53 +1,67 @@
 import ffmpeg
 import os
 
-def main(scale: list = [512, 512],
-         output_folder: str = "outputs",
-         framerate: int = 30,
-         codec: str = "libvpx-vp9",
-         ss: str = "00:00:00",
-         to: str = "00:00:03"):
+from parser import parser
 
-    for file in os.listdir("../media/inputs/"):
+def main(scale: list,
+         output_folder: str,
+         framerate: int,
+         codec: str,
+         ss: str,
+         to: str):
+
+    root_dir = os.getcwd()
+
+    for file in os.listdir(f'{root_dir}/media/inputs'):
         if file == '.gitkeep': continue
 
         print(file.split('.')[0])
         try:
-            input = ffmpeg.input(f"../media/inputs/{file}",
+            input = ffmpeg.input(f'{root_dir}/media/inputs/{file}',
                                  ss=ss,
                                  to=to,
                                  )
 
             output = (input
-                      .filter("scale", scale[0], -1)
-                      .output(f"../media/{output_folder}/{file.split('.')[0]}.webm",
+                      .filter('scale', scale[0], -1)
+                      .output(f'{root_dir}/media/{output_folder}/{file.split(".")[0]}.webm',
                               r=framerate,
                               vcodec=codec,
                               **{
-                                  "b:v": "500K",
-                                  "maxrate": "500K"
+                                  'b:v': '500K',
+                                  'maxrate': '500K'
                               }
                               )
                       )
 
             output.run(overwrite_output=True)
         except ffmpeg.Error as e:
-            print("STDOUT: ", e.stdout)
-            print("STDERR: ", e.stderr)
+            print('STDOUT: ', e.stdout)
+            print('STDERR: ', e.stderr)
 
 
 def add_text(input, text: str):
-    input.filter("drawtext",
+    input.filter('drawtext',
           text=text,
           x=32,
           y=450,
           fontsize=48,
-          fontcolor="white"
+          fontcolor='white'
           )
 
     return input
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(
+        scale=[
+            args.scale_x,
+            args.scale_y],
+         output_folder=args.output_folder,
+         codec=args.codec,
+         framerate=args.framerate,
+         ss=args.start_from,
+         to=args.end
+         )
     
---
+++ src/parser.py
@@ -0,0 +1,21 @@
+import argparse
+import tomllib
+import os
+
+root_dir = os.getcwd()
+
+with open(f'{root_dir}/configs/defaults.toml', 'rb') as f:
+    config = tomllib.load(f)
+
+parser = argparse.ArgumentParser(
+    prog='mconverter',
+    description='Media Converter that i use to create telegram stickers'
+)
+
+parser.add_argument('-sx', '--scale_x', type=int, default=config['scale_x'])
+parser.add_argument('-sy', '--scale_y', type=int, default=config['scale_y'])
+parser.add_argument('-of', '--output_folder', type=str, default=config['output_folder'])
+parser.add_argument('-c', '--codec', type=str, default=config['codec'])
+parser.add_argument('-fr', '--framerate', type=int, default=config['framerate'])
+parser.add_argument('-sf', '--start_from', type=str, default=config['start_from'])
+parser.add_argument('-e', '--end', type=str, default=config['end'])
