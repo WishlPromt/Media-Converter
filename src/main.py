210d3074961d9c8c944ec3481a5c731ffbba4e06
Wishl Promt <igorbedniagin1@gmail.com>
2025-05-17 10:19:28 +0500

    Image convert

--- src/main.py
+++ src/main.py
@@ -1,43 +1,109 @@
 import ffmpeg
 import os
+import filetype
 
 from parser import parser
 
-def main(scale: list,
-         output_folder: str,
+def check_filetype(file_path: str):
+    ext = filetype.guess(file_path)
+
+    if ext is None:
+        return False
+    elif ext.mime.startswith('video/'):
+        return 'video'
+    elif ext.mime.startswith('image/'):
+        return 'image'
+    else:
+        return False
+
+def main(scale,
+         output_format: str,
          framerate: int,
          codec: str,
          ss: str,
          to: str):
 
+    if not scale:
+        scale = [-1, -1]
     root_dir = os.getcwd()
 
     for file in os.listdir(f'{root_dir}/media/inputs'):
         if file == '.gitkeep': continue
 
+        filepath = f'{root_dir}/media/inputs/{file}'
+
         print(file.split('.')[0])
-        try:
-            input = ffmpeg.input(f'{root_dir}/media/inputs/{file}',
-                                 ss=ss,
-                                 to=to,
-                                 )
 
-            output = (input
-                      .filter('scale', scale[0], -1)
-                      .output(f'{root_dir}/media/{output_folder}/{file.split(".")[0]}.webm',
-                              r=framerate,
-                              vcodec=codec,
-                              **{
-                                  'b:v': '500K',
-                                  'maxrate': '500K'
-                              }
-                              )
-                      )
+        type_of_file = check_filetype(filepath)
+        if type_of_file:
+            match type_of_file:
+                case 'video':
+                    convert_video(
+                        filepath=filepath,
+                        ss=ss,
+                        to=to,
+                        scale_x=scale[0],
+                        scale_y=scale[1],
+                        root_dir=root_dir,
+                        file=file,
+                        output_format=output_format,
+                        framerate=framerate,
+                        codec=codec
+                    )
+                case 'image':
+                    convert_image(filepath=filepath,
+                                  scale_x=scale[0],
+                                  scale_y=scale[1],
+                                  output_format=output_format,
+                                  file=file,
+                                  root_dir=root_dir
+                                  )
 
-            output.run(overwrite_output=True)
-        except ffmpeg.Error as e:
-            print('STDOUT: ', e.stdout)
-            print('STDERR: ', e.stderr)
+
+
+
+def convert_video(**params):
+
+    try:
+        input = ffmpeg.input(params['filepath'],
+                             ss=params['ss'],
+                             to=params['to'],
+                             )
+
+        output = (input
+                  .filter('scale', params['scale_x'], params['scale_y'])
+                  .output(f'{params['root_dir']}/media/outputs/{params['file'].split(".")[0]}.{params['output_format']}',
+                          r=params['framerate'],
+                          vcodec=params['codec'],
+                          **{
+                              'b:v': '500K',
+                              'maxrate': '500K'
+                          }
+                          )
+                  )
+
+        output.run(overwrite_output=True)
+    except ffmpeg.Error as e:
+        print('STDOUT: ', e.stdout)
+        print('STDERR: ', e.stderr)
+
+
+def convert_image(**params):
+    try:
+        input = ffmpeg.input(params['filepath'])
+
+        output = (input
+                  .filter('scale', params['scale_x'], params['scale_y'])
+                  .output(f'{params['root_dir']}/media/outputs/{params['file'].split(".")[0]}.{params['output_format']}',
+                          **{
+                              'q:v': '5'
+                          })
+                  )
+
+        output.run(overwrite_output=True)
+    except ffmpeg.Error as e:
+        print('STDOUT: ', e.stdout)
+        print('STDERR: ', e.stderr)
 
 
 def add_text(input, text: str):
@@ -57,8 +123,8 @@ if __name__ == '__main__':
     main(
         scale=[
             args.scale_x,
-            args.scale_y],
-         output_folder=args.output_folder,
+            args.scale_y] if not args.original_scale else False,
+         output_format=args.output_format,
          codec=args.codec,
          framerate=args.framerate,
          ss=args.start_from,
--- src/parser.py
+++ src/parser.py
@@ -14,7 +14,8 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-sx', '--scale_x', type=int, default=config['scale_x'])
 parser.add_argument('-sy', '--scale_y', type=int, default=config['scale_y'])
-parser.add_argument('-of', '--output_folder', type=str, default=config['output_folder'])
+parser.add_argument('-os', '--original_scale', action='store_true')
+parser.add_argument('-of', '--output_format', type=str, default=config['output_format'])
 parser.add_argument('-c', '--codec', type=str, default=config['codec'])
 parser.add_argument('-fr', '--framerate', type=int, default=config['framerate'])
 parser.add_argument('-sf', '--start_from', type=str, default=config['start_from'])
